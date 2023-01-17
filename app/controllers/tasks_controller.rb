23092afabca09ea218646143724496fb6106bb8e
nikki dekerf <nikkidekerf@gmail.com>
2023-01-17 12:48:55 +0100

    POST method now works for the form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def create
+    Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
